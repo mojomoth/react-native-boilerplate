77138ad4c27a5984a091ae96ca48fcb53ae81b23
ljy <>
2018-01-22 18:47:49 +0900

    Setup Example for react-native-navigation

---
+++ src/pages/PushedScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class PushedScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Pushed</Text>
+      </View>
+    );
+  }
+}
---
+++ src/pages/SecondTabScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class SecondTabScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Second Tab</Text>
+      </View>
+    );
+  }
+}
